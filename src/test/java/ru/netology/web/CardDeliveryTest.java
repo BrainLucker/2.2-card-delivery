d7c241c590bde9d8477d9c889ace649922cd86ec
Maksim Ivlev <brainlucker@gmail.com>
2022-03-28 23:41:45 +0300

    Test with complex elements added

--- src/test/java/ru/netology/web/CardDeliveryTest.java
+++ src/test/java/ru/netology/web/CardDeliveryTest.java
@@ -21,6 +21,12 @@ public class CardDeliveryTest {
         return formatter.format(calendar.getTime());
     }
 
+    boolean isNextMonth(Integer dayOfDelivery) {
+        Calendar calendar = new GregorianCalendar();
+        int today = calendar.get(Calendar.DAY_OF_MONTH);
+        return (dayOfDelivery < today);
+    }
+
     @BeforeEach
     void setUp() {
         Configuration.holdBrowserOpen = true;
@@ -36,6 +42,30 @@ public class CardDeliveryTest {
         SelenideElement form = $(".form");
         form.$("[data-test-id=city] input").val("Нарьян-Мар");
         form.$("[data-test-id=date] input.input__control").doubleClick().sendKeys(deliveryDate);
+        form.$("[data-test-id=name] input").val("Уильям Мак-Кинли");
+        form.$("[data-test-id=phone] input").val("+79991234567");
+        form.$("[data-test-id=agreement] .checkbox__box").click();
+        form.$$("button").find(exactText("Забронировать")).click();
+
+        $("[data-test-id=notification] .notification__content").should(appear, Duration.ofSeconds(15)).shouldHave(text(notificationText));
+    }
+
+    @Test
+    void shouldSubmitValidFormComplexElements() {
+        String city = "Казань";
+        String deliveryDate = setDeliveryDate(7); // задаем дату доставки через неделю
+        Integer dayOfDelivery = Integer.parseInt(deliveryDate.substring(0, 2)); // получаем число месяца из даты доставки
+        String notificationText = "Встреча успешно забронирована на " + deliveryDate;
+
+        SelenideElement form = $(".form");
+        SelenideElement calendar = $(".popup .calendar");
+        form.$("[data-test-id=city] input").val(city.substring(0, 2)); // вводим первые две буквы города
+        $$(".input__popup .menu-item__control").find(exactText(city)).click();
+        form.$("[data-test-id=date] button").click();
+        if (isNextMonth(dayOfDelivery)) { // проверяем нужно ли перелистнуть на следующий месяц в виджете календаря
+            calendar.$("[data-step='1']").click();
+        }
+        calendar.$$(".calendar__day").find(exactText(dayOfDelivery.toString())).click();
         form.$("[data-test-id=name] input").val("Уильям Мак-Кинли");
         form.$("[data-test-id=phone] input").val("+79991234567");
         form.$("[data-test-id=agreement] .checkbox__box").click();
